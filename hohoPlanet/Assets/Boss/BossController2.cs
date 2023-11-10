18d2b5a40948eb3bc80bd687b5b37615e59e65b7
RyuHaJung <red061011@gmail.com>
2023-11-10 23:06:12 +0900

    Feat:" 보스2 스크립트 수정"
    공격 패턴 추가

--- hohoPlanet/Assets/Boss/BossController2.cs
+++ hohoPlanet/Assets/Boss/BossController2.cs
@@ -4,58 +4,68 @@ using UnityEngine;
 
 public class BossController2 : MonoBehaviour
 {
-    // 체력
     public int hp = 10;
-    // 반응 거리
     public float reactionDistance = 7.0f;
 
-    public GameObject bulletPrefab;     //총알
-    public float shootSpeed = 5.0f;     //총알 속도
+    public GameObject bulletPrefab;     // 총알
+    public float shootSpeed = 5.0f;     // 총알 속도
 
-    //공격중인지 여부
     bool inAttack = false;
 
-    //내가 추가 한거
-    public float barrageSpeed = 5.0f; //탄막 속도
-    public int numberOfProjectiles = 5; //발사할 총알 개수
+    // 내가 추가한 부분
+    public float barrageSpeed = 5.0f; // 탄막 속도
+    public int numberOfProjectilesPattern1 = 8; // 1번째 패턴의 발사할 총알 개수
+    public int numberOfProjectilesPattern2 = 10; // 2번째 패턴의 발사할 총알 개수
 
-    // Use this for initialization
-    void Start()
-    {
-    }
+    private bool isPattern1 = true;
 
-    // Update is called once per frame
     void Update()
     {
         if (hp > 0)
         {
-            //Player 게임 오브젝트 가져오기
             GameObject player = GameObject.FindGameObjectWithTag("Player");
             if (player != null)
             {
-                //플레이어와의 거리 확인
                 Vector3 plpos = player.transform.position;
                 float dist = Vector2.Distance(transform.position, plpos);
-                if (dist <= reactionDistance && inAttack == false)
+
+                if (dist <= reactionDistance && !inAttack)
                 {
-                    //범위 안 & 공격 중이 아니면 공격 애니메이션
                     inAttack = true;
-                    // 애니메이션 변경
                     GetComponent<Animator>().Play("BossAttack");
+
+                    // 패턴 변경
+                    StartCoroutine(SwitchAttackPattern());
                 }
                 else if (dist > reactionDistance && inAttack)
                 {
                     inAttack = false;
-                    // 애니메이션 변경
                     GetComponent<Animator>().Play("BossIdle");
+                    // 패턴 취소
+                    StopAllCoroutines();
                 }
             }
             else
             {
                 inAttack = false;
-                // 애니메이션 변경
                 GetComponent<Animator>().Play("BossIdle");
+                // 패턴 취소
+                StopAllCoroutines();
             }
+        }
+    }
+
+    IEnumerator SwitchAttackPattern()
+    {
+        while (true)
+        {
+            // 1번째 패턴 실행
+            Attack();
+            yield return new WaitForSeconds(3f); // 3초 간격으로 패턴 변경
+
+            // 2번째 패턴 실행
+            Attack();
+            yield return new WaitForSeconds(3f); // 3초 간격으로 패턴 변경
         }
     }
 
@@ -63,52 +73,79 @@ public class BossController2 : MonoBehaviour
     {
         if (collision.gameObject.tag == "Arrow")
         {
-            //데미지
             hp--;
             if (hp <= 0)
             {
-                //사망！
-                //충돌 판정 비활성
+                // 사망!
                 GetComponent<CircleCollider2D>().enabled = false;
-                // 애니메이션 변경
                 GetComponent<Animator>().Play("BossDead");
-                //1초 뒤에 제거
                 Destroy(gameObject, 1);
-
-                //SE재생 (보스 사망)
-                //SoundManager.soundManager.SEPlay(SEType.BossKilled);
             }
         }
     }
-    //공격
-    void Attack()
+
+    // 1번째 패턴
+    void AttackPattern1()
     {
-        //발사 위치 오브젝트 가져오기
         Transform tr = transform.Find("gate");
         GameObject gate = tr.gameObject;
 
-        //플레이어 찾기
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
-        if (player != null)
+        for (int i = 0; i < numberOfProjectilesPattern1; i++)
         {
-            for (int i = 0; i < numberOfProjectiles; i++)
-            {
-                float angle = i * (360 / numberOfProjectiles); //탄막의 각도를 조절
+            float angle = i * (360f / numberOfProjectilesPattern1);
 
-                //Prefab으로 총알 오브젝트 만들기(진행 방향으로 회전)
-                Quaternion r = Quaternion.Euler(0, 0, angle);
-                GameObject bullet = Instantiate(bulletPrefab, gate.transform.position, r);
+            Quaternion r = Quaternion.Euler(0, 0, angle);
+            GameObject bullet = Instantiate(bulletPrefab, gate.transform.position, r);
 
-                //탄막 속도와 방향 설정
-                float rad = angle * Mathf.Deg2Rad;
-                float x = Mathf.Cos(rad);
-                float y = Mathf.Sin(rad);
-                Vector3 v = new Vector3(x, y) * barrageSpeed;
+            float rad = angle * Mathf.Deg2Rad;
+            float x = Mathf.Cos(rad);
+            float y = Mathf.Sin(rad);
+            Vector3 v = new Vector3(x, y) * barrageSpeed;
 
-                //발사
-                Rigidbody2D rbody = bullet.GetComponent<Rigidbody2D>();
-                rbody.AddForce(v, ForceMode2D.Impulse);
-            }
+            Rigidbody2D rbody = bullet.GetComponent<Rigidbody2D>();
+            rbody.AddForce(v, ForceMode2D.Impulse);
         }
+    }
+
+    // 2번째 패턴
+    void AttackPattern2()
+    {
+        Transform tr = transform.Find("gate");
+        GameObject gate = tr.gameObject;
+
+        for (int i = 0; i < numberOfProjectilesPattern2; i++)
+        {
+            float angle = i * (360f / numberOfProjectilesPattern2);
+
+            Quaternion r = Quaternion.Euler(0, 0, angle);
+            GameObject bullet = Instantiate(bulletPrefab, gate.transform.position, r);
+
+            float rad = angle * Mathf.Deg2Rad;
+            float x = Mathf.Cos(rad);
+            float y = Mathf.Sin(rad);
+            Vector3 v = new Vector3(x, y) * barrageSpeed;
+
+            Rigidbody2D rbody = bullet.GetComponent<Rigidbody2D>();
+            rbody.AddForce(v, ForceMode2D.Impulse);
+        }
+    }
+
+    // 공격
+    void Attack()
+    {
+        // 패턴에 따라 다른 공격 실행
+        if (isPattern1)
+        {
+            // 첫 번째 패턴 실행
+            AttackPattern1();
+        }
+        else
+        {
+            // 두 번째 패턴 실행
+            AttackPattern2();
+        }
+
+        // 패턴 변경
+        isPattern1 = !isPattern1;
     }
 }
